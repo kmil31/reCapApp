4f04df9c445e14a4bce4342bc6fe070a11b93d65
han <67534560+hannahhuda@users.noreply.github.com>
2021-04-29 06:53:20 +0800

    Update incomezakat.dart

--- lib/screens/zakat/incomezakat.dart
+++ lib/screens/zakat/incomezakat.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:recap/sharedwidgets/drawer.dart';
+import 'package:editable/editable.dart';
 
 class IncomeZakat extends StatefulWidget {
   @override
@@ -10,37 +11,97 @@ class IncomeZakatState extends State<IncomeZakat> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          titleSpacing: 5.0,
-          elevation: 5.0,
-          backgroundColor: Color(0xff201F23),
-          title: Text('Income Zakat'),
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.black),
+        title: Text(
+          "Income Zakat",
+          style: TextStyle(
+            color: Colors.black,
+          ),
         ),
-        drawer: DrawerMenu(),
-        body: Center(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-              Container(
-                alignment: Alignment.topLeft,
-                margin: EdgeInsets.all(20),
-                child: Text('Zakat al-Fitr',
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black)),
+        titleSpacing: 3.0,
+        elevation: 5.0,
+        backgroundColor: Colors.white,
+      ),
+      drawer: DrawerMenu(),
+      body: Center(
+        child: ListView(
+          //mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              alignment: Alignment.topLeft,
+              margin: EdgeInsets.all(20),
+              child: Text('Income Zakat',
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black)),
+            ),
+            Container(
+              margin: EdgeInsets.all(20),
+              alignment: Alignment.center,
+              width: double.infinity,
+              height: 90,
+              decoration: BoxDecoration(
+                color: Colors.green[50],
+                borderRadius: BorderRadius.circular(24.0),
               ),
-              Container(
-                color: Colors.green[100],
-                margin: EdgeInsets.all(20),
-                alignment: Alignment.center,
-                width: double.infinity,
-                height: 200,
-                padding: EdgeInsets.symmetric(vertical: 16),
-                child: Text(
-                    'Income zakat is all types of wages, remuneration, payments earned from work or efforts undertaken either on a regular basis or once in a while.',
-                    style: TextStyle(fontSize: 15, color: Colors.black)),
-              ),
-            ])));
+              padding: EdgeInsets.symmetric(vertical: 16),
+              child: Text(
+                  'Income zakat is all types of wages, remuneration, payments earned from work or efforts undertaken either on a regular basis or once in a while.',
+                  style: TextStyle(fontSize: 15, color: Colors.black)),
+            ),
+            Text("A: INCOME COMPONENTS",
+                style: TextStyle(fontSize: 15, color: Colors.black)),
+            DataTable(
+              columns: [
+                DataColumn(
+                    label: Text('NO',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold))),
+                DataColumn(
+                    label: Text('BILL DETAILS',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold))),
+                DataColumn(
+                    label: Text('RM',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold))),
+                DataColumn(
+                    label: Text(' ',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold))),
+              ],
+              rows: [
+                DataRow(cells: [
+                  DataCell(Text('A1')),
+                  DataCell(Text('Annual Income')),
+                  DataCell(Text('MYR')),
+                  DataCell(Text(' ')),
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('A2')),
+                  DataCell(Text('Side Income')),
+                  DataCell(Text('MYR')),
+                  DataCell(Text(' ')),
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('A3')),
+                  DataCell(Text('Al-Mustaghallat income')),
+                  DataCell(Text('MYR')),
+                  DataCell(Text(' ')),
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('A4')),
+                  DataCell(Text('Donations or Contribution')),
+                  DataCell(Text('MYR')),
+                  DataCell(Text(' ')),
+                ]),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
